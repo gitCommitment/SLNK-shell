2b6302493e6df5321349685df4d3514b07ebfb4e
jwerner <jordancodywerner@gmail.com>
2025-10-25 13:47:37 -0700

    refactoring

--- src/main.cs
+++ src/main.cs
@@ -2,36 +2,35 @@ using SLNK_Shell.codecrafters_shell_csharp.src;
 using System.Security.Cryptography;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static int Main()
     {
-        //while (true)
-        //{            
-        //    Console.WriteLine("$ ");
-        //    var cmd = Console.ReadLine();
-
-        //    if (cmd == "exit")
-        //    {
-        //        return 0;
-        //    }
-        //    else
-        //    {
-        //        Console.ForegroundColor = ConsoleColor.Red;
-        //        Console.WriteLine($"{cmd}: command not found");
-        //        Console.ResetColor();
-        //    }
-        //}
-
         while (true)
         {
-            Console.Write("$ ");
+            Console.WriteLine("$ ");
             var cmd = Console.ReadLine();
-            if (cmd == "exit 0")
+
+            if (cmd == "exit")
+            {
                 return 0;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{cmd}: command not found");
+            Console.ResetColor();
+            
         }
+
+        //while (true)
+        //{
+        //    Console.Write("$ ");
+        //    var cmd = Console.ReadLine();
+        //    if (cmd == "exit 0")
+        //        return 0;
+        //    Console.WriteLine($"{cmd}: command not found");
+        //}
 
 
     }
