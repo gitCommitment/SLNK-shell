773dd0ea7604c794bb324cd7247719b9183b6a7f
jwerner <jordancodywerner@gmail.com>
2025-10-25 13:52:25 -0700

    refactoring

--- src/main.cs
+++ src/main.cs
@@ -10,7 +10,7 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("$ ");
+            Console.Write("$ ");
             var cmd = Console.ReadLine();
 
             if (cmd == "exit 0")
