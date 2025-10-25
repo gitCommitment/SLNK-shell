af90e192f0d5d3e26cac732f23a8139d843a3294
jwerner <jordancodywerner@gmail.com>
2025-10-25 13:36:13 -0700

    refactoring

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -8,35 +8,6 @@ namespace SLNK_Shell.codecrafters_shell_csharp.src
 {
     internal class Utilities
     {
-        private int exitNumber;
-        private bool exit = false;
-
-        public int ExitNumber { get; set; }
-        public static bool Exit { get; set; }
-
-        public static void Prompt()
-        {
-            string? cmd;
-            
-            while (!Exit)
-            {
-
-                Console.Write("$ ");
-                cmd = Console.ReadLine();
-                if (cmd == "exit 0")
-                {
-                    Exit = true;
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{cmd}: command not found");
-                    Console.ResetColor();
-                }
-            }
-
-        }
         
         
 
