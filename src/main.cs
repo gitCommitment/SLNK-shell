0af8663a842dec9bf335acf7187e270a2960fffb
jwerner <jordancodywerner@gmail.com>
2025-10-24 21:38:22 -0700

    stage 2

--- src/main.cs
+++ src/main.cs
@@ -2,7 +2,7 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-        // Console.Write("$ ");
+    //TODO: Uncomment the code below to pass the first stage
+         Console.Write("$ ");
     }
 }
